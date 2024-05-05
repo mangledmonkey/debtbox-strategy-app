b6ae814632e6cb3e3cfad947f0f27f76f9a92f56
Brandon West <brandon@mangledmonkeymedia.com>
2024-05-05 18:50:33 -0400

    Attempted eslint updates.

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+	root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier',
+		'plugin:storybook/recommended'
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: [
+		'@stylistic'
+	],
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte']
+	},
+	env: {
+		browser: true,
+		es2017: true,
+		node: true
+	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
+	rules: {
+		'@stylistic/ts/semi': 'error',
+		'@stylistic/no-explicit-any': 'warn'
+	}
+};
